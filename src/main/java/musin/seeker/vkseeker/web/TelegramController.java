bf792f0b404359a6742c2b10525485cd796ed3df
Rustam Musin <PloadyFree@gmail.com>
2019-07-04 08:32:05 +0400

    fix [0]

--- src/main/java/musin/seeker/vkseeker/web/TelegramController.java
+++ src/main/java/musin/seeker/vkseeker/web/TelegramController.java
@@ -1,6 +1,7 @@
 package musin.seeker.vkseeker.web;
 
 import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -11,7 +12,7 @@ public class TelegramController {
 
     @RequestMapping("/telegram")
     @SneakyThrows
-    public SendMessage telegram(Update update) {
+    public SendMessage telegram(@RequestBody Update update) {
         return new SendMessage(update.getMessage().getChatId(), "yes, i'm here");
     }
 }
