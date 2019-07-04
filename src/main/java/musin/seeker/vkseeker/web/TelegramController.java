e7e390ad001b61090745a3d5b01104fbef68af41
Rustam Musin <PloadyFree@gmail.com>
2019-07-04 08:29:29 +0400

    Подправил вебхук, теперь должен отвечать

--- src/main/java/musin/seeker/vkseeker/web/TelegramController.java
+++ src/main/java/musin/seeker/vkseeker/web/TelegramController.java
@@ -3,13 +3,15 @@ package musin.seeker.vkseeker.web;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
 public class TelegramController {
 
     @RequestMapping("/telegram")
     @SneakyThrows
-    public String telegram() {
-        return "yes i'm here";
+    public SendMessage telegram(Update update) {
+        return new SendMessage(update.getMessage().getChatId(), "yes, i'm here");
     }
 }
