538bc623696504d969eb454d82ed52c420864689
Rustam Musin <PloadyFree@gmail.com>
2019-07-28 11:18:44 +0400

    Упростил получение разницы между списками

--- src/main/java/musin/seeker/vkseeker/RelationList.java
+++ src/main/java/musin/seeker/vkseeker/RelationList.java
@@ -5,25 +5,31 @@ import musin.seeker.vkseeker.db.model.RelationChange;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static musin.seeker.vkseeker.db.model.RelationType.*;
 
 @AllArgsConstructor
 public class RelationList {
     private final int owner;
-    private final Set<Integer> friends = new TreeSet<>();
-    private final Set<Integer> followers = new TreeSet<>();
+    private final Map<String, Set<Integer>> relationTypeToId;
+
+    public RelationList(int owner) {
+        this.owner = owner;
+        relationTypeToId = new HashMap<>();
+        relationTypeToId.put(FRIEND, new TreeSet<>());
+        relationTypeToId.put(FOLLOWER, new TreeSet<>());
+    }
 
     public void applyChange(RelationChange change) {
-        Integer id = change.getTarget();
+        int id = change.getTarget();
 
         String prevType = change.getPrevType();
-        if (FRIEND.equals(prevType)) friends.remove(id);
-        if (FOLLOWER.equals(prevType)) followers.remove(id);
+        relationTypeToId.get(prevType).remove(id);
 
         String curType = change.getCurType();
-        if (FRIEND.equals(curType)) friends.add(id);
-        if (FOLLOWER.equals(curType)) followers.add(id);
+        relationTypeToId.get(curType).add(id);
     }
 
     public void applyChanges(Iterable<RelationChange> changes) {
@@ -31,33 +37,33 @@ public class RelationList {
     }
 
     public List<RelationChange> getDifference(RelationList other) {
-        Set<Integer> allIds = new TreeSet<>();
-        allIds.addAll(friends);
-        allIds.addAll(followers);
-        allIds.addAll(other.friends);
-        allIds.addAll(other.followers);
-
         LocalDateTime time = LocalDateTime.now();
-        List<RelationChange> result = new ArrayList<>();
-        for (int id : allIds) {
-            String wasType = getType(id);
-            String nowType = other.getType(id);
-            if (wasType.equals(nowType)) continue;
-            result.add(RelationChange.builder()
-                    .owner(owner)
-                    .target(id)
-                    .prevType(wasType)
-                    .curType(nowType)
-                    .time(time)
-                    .build()
-            );
-        }
-        return result;
+        return getAllIds(this, other)
+                .map(id -> RelationChange.builder()
+                        .owner(owner)
+                        .target(id)
+                        .prevType(getType(id))
+                        .curType(other.getType(id))
+                        .time(time)
+                        .build())
+                .filter(r -> !r.getPrevType().equals(r.getCurType()))
+                .collect(Collectors.toList());
+    }
+
+    private static Stream<Integer> getAllIds(RelationList... lists) {
+        return Arrays.stream(lists)
+                .flatMap(RelationList::getIds);
+    }
+
+    private Stream<Integer> getIds() {
+        return relationTypeToId.values().stream()
+                .flatMap(Collection::stream);
     }
 
     private String getType(int id) {
-        if (friends.contains(id)) return FRIEND;
-        if (followers.contains(id)) return FOLLOWER;
+        for (Map.Entry<String, Set<Integer>> e : relationTypeToId.entrySet())
+            if (e.getValue().contains(id))
+                return e.getKey();
         return NONE;
     }
 
