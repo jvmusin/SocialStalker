ce0f7399d311c7cae94decfc93d859e116b1f715
Rustam Musin <PloadyFree@gmail.com>
2020-05-19 11:04:55 +0400

    Update vk api version and access token

--- src/main/java/musin/seeker/vkseeker/api/MusinUserActor.java
+++ src/main/java/musin/seeker/vkseeker/api/MusinUserActor.java
@@ -4,7 +4,7 @@ import com.vk.api.sdk.client.actors.UserActor;
 
 public class MusinUserActor extends UserActor {
     private static final int USER_ID = 39666902;
-    private static final String ACCESS_TOKEN = "2f8e6773252d2d2b088caa290cd2e38aad2ef15b9df0e0dade25bbd76f1894452b401697f907fd22b25e7";
+    private static final String ACCESS_TOKEN = "935a4239ad41716de2b9e0702d799827d495abc787ef8f020a0330a7c93727ee685de6d2b8c6cb02ebf6f";
 
     public MusinUserActor() {
         super(USER_ID, ACCESS_TOKEN);
