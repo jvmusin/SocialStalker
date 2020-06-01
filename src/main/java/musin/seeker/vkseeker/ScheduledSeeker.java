d492acb42c061c2e2a1c97a2676ae7fe002e3952
Rustam Musin <PloadyFree@gmail.com>
2020-06-01 22:16:40 +0400

    Allow do up to five request per second and use parallel requests in ScheduledSeeker (+reindent)

--- src/main/java/musin/seeker/vkseeker/ScheduledSeeker.java
+++ src/main/java/musin/seeker/vkseeker/ScheduledSeeker.java
@@ -2,44 +2,49 @@ package musin.seeker.vkseeker;
 
 import lombok.AllArgsConstructor;
 import musin.seeker.vkseeker.api.VkApi;
-import musin.seeker.vkseeker.db.model.RelationChange;
 import musin.seeker.vkseeker.db.RelationChangeService;
 import musin.seeker.vkseeker.db.SeekerService;
+import musin.seeker.vkseeker.db.model.RelationChange;
+import musin.seeker.vkseeker.db.model.Seeker;
 import musin.seeker.vkseeker.telegram.ChangesNotifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static musin.seeker.vkseeker.db.model.RelationType.*;
+import static musin.seeker.vkseeker.db.model.RelationType.FOLLOWER;
+import static musin.seeker.vkseeker.db.model.RelationType.FRIEND;
 
 @Service
 @AllArgsConstructor
 public class ScheduledSeeker {
 
-    private final SeekerService seekerService;
-    private final RelationChangeService relationChangeService;
-    private final VkApi vkApi;
-    private final ChangesNotifier changesNotifier;
+  private final SeekerService seekerService;
+  private final RelationChangeService relationChangeService;
+  private final VkApi vkApi;
+  private final ChangesNotifier changesNotifier;
 
-    @Scheduled(fixedDelay = 1000L * 60 * 10)
-    public void run() {
-        seekerService.findAll().forEach(seeker -> run(seeker.getOwner()));
-    }
+  @Scheduled(fixedDelay = 1000L * 60 * 10)
+  public void run() {
+    seekerService.findAll().stream()
+        .parallel()
+        .map(Seeker::getOwner)
+        .forEach(this::run);
+  }
 
-    private void run(int owner) {
-        final List<RelationChange> changes = relationChangeService.findAllByOwner(owner);
-        final RelationList was = new RelationList(owner);
-        was.applyChanges(changes);
+  private void run(int owner) {
+    List<RelationChange> changes = relationChangeService.findAllByOwner(owner);
+    RelationList was = new RelationList(owner);
+    was.applyChanges(changes);
 
-        final List<Integer> friends = vkApi.loadFriends(owner);
-        final List<Integer> followers = vkApi.loadFollowers(owner);
-        final RelationList now = new RelationList(owner);
-        friends.forEach(id -> now.applyChange(RelationChange.builder().curType(FRIEND).target(id).build()));
-        followers.forEach(id -> now.applyChange(RelationChange.builder().curType(FOLLOWER).target(id).build()));
+    List<Integer> friends = vkApi.loadFriends(owner);
+    List<Integer> followers = vkApi.loadFollowers(owner);
+    RelationList now = new RelationList(owner);
+    friends.forEach(id -> now.applyChange(RelationChange.builder().curType(FRIEND).target(id).build()));
+    followers.forEach(id -> now.applyChange(RelationChange.builder().curType(FOLLOWER).target(id).build()));
 
-        final List<RelationChange> difference = was.getDifference(now);
-        relationChangeService.saveAll(difference);
-        changesNotifier.notify(difference);
-    }
+    List<RelationChange> difference = was.getDifference(now);
+    relationChangeService.saveAll(difference);
+    changesNotifier.notify(difference);
+  }
 }
--- src/main/java/musin/seeker/vkseeker/api/MyHttpTransportClient.java
+++ src/main/java/musin/seeker/vkseeker/api/MyHttpTransportClient.java
@@ -6,75 +6,70 @@ import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
-import java.util.concurrent.locks.ReentrantLock;
-
-import static java.time.LocalDateTime.now;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class MyHttpTransportClient extends HttpTransportClient {
 
-    private LocalDateTime lastTimeUsed = now();
-    private final Duration minDelay = Duration.ofMillis(1000);
-    private final ReentrantLock lock = new ReentrantLock(true);
+  private static final int DELAY_BETWEEN_REQUESTS_MILLIS = 1000;
+  private static final int REQUESTS_PER_SECOND = 5;
+  private final Semaphore availableRequestsSemaphore = new Semaphore(REQUESTS_PER_SECOND, true);
 
-    public MyHttpTransportClient() {
-    }
+  public MyHttpTransportClient() {
+  }
 
-    public MyHttpTransportClient(int retryAttemptsNetworkErrorCount, int retryAttemptsInvalidStatusCount) {
-        super(retryAttemptsNetworkErrorCount, retryAttemptsInvalidStatusCount);
-    }
+  public MyHttpTransportClient(int retryAttemptsNetworkErrorCount, int retryAttemptsInvalidStatusCount) {
+    super(retryAttemptsNetworkErrorCount, retryAttemptsInvalidStatusCount);
+  }
 
-    @SneakyThrows
-    private <T> T execute(Callable<T> work) {
-        try {
-            lock.lock();
-            Duration d = Duration.between(lastTimeUsed, now());
-            long sleepMillis = minDelay.minus(d).toMillis();
-            if (sleepMillis > 0) Thread.sleep(sleepMillis);
-            return work.call();
-        } finally {
-            lastTimeUsed = now();
-            lock.unlock();
-        }
+  @SneakyThrows
+  private <T> T execute(Callable<T> work) {
+    try {
+      availableRequestsSemaphore.acquireUninterruptibly();
+      return work.call();
+    } finally {
+      CompletableFuture.delayedExecutor(DELAY_BETWEEN_REQUESTS_MILLIS, TimeUnit.MILLISECONDS)
+          .execute(availableRequestsSemaphore::release);
     }
+  }
 
-    @Override
-    public ClientResponse get(String url) {
-        return execute(() -> super.get(url));
-    }
+  @Override
+  public ClientResponse get(String url) {
+    return execute(() -> super.get(url));
+  }
 
-    public ClientResponse post(String url, String body) {
-        return execute(() -> super.post(url, body));
-    }
+  public ClientResponse post(String url, String body) {
+    return execute(() -> super.post(url, body));
+  }
 
-    public ClientResponse post(String url, String fileName, File file) {
-        return execute(() -> super.post(url, fileName, file));
-    }
+  public ClientResponse post(String url, String fileName, File file) {
+    return execute(() -> super.post(url, fileName, file));
+  }
 
-    public ClientResponse post(String url, String body, String contentType) {
-        return execute(() -> super.post(url, body, contentType));
-    }
+  public ClientResponse post(String url, String body, String contentType) {
+    return execute(() -> super.post(url, body, contentType));
+  }
 
-    public ClientResponse get(String url, String contentType) {
-        return execute(() -> super.get(url, contentType));
-    }
+  public ClientResponse get(String url, String contentType) {
+    return execute(() -> super.get(url, contentType));
+  }
 
-    public ClientResponse post(String url) {
-        return execute(() -> super.post(url));
-    }
+  public ClientResponse post(String url) {
+    return execute(() -> super.post(url));
+  }
 
-    public ClientResponse delete(String url) {
-        return execute(() -> super.delete(url));
-    }
+  public ClientResponse delete(String url) {
+    return execute(() -> super.delete(url));
+  }
 
-    public ClientResponse delete(String url, String body) {
-        return execute(() -> super.delete(url, body));
-    }
+  public ClientResponse delete(String url, String body) {
+    return execute(() -> super.delete(url, body));
+  }
 
-    public ClientResponse delete(String url, String body, String contentType) {
-        return execute(() -> super.delete(url, body, contentType));
-    }
+  public ClientResponse delete(String url, String body, String contentType) {
+    return execute(() -> super.delete(url, body, contentType));
+  }
 }
