6071c32420ec85e90ebed9dcaba0dbe2e579d40e
Rustam Musin <PloadyFree@gmail.com>
2020-06-02 10:34:26 +0400

    Add async methods to vk api

--- src/main/java/musin/seeker/vkseeker/api/VkApi.java
+++ src/main/java/musin/seeker/vkseeker/api/VkApi.java
@@ -1,16 +1,23 @@
 package musin.seeker.vkseeker.api;
 
+import musin.seeker.vkseeker.RelationList;
 import musin.seeker.vkseeker.db.model.RelationChange;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public interface VkApi {
-    List<Integer> loadFriends(int userId);
-    List<Integer> loadFollowers(int userId);
+  List<Integer> loadFriends(int userId);
+  List<Integer> loadFollowers(int userId);
+  SimpleVkUser loadUser(int userId);
+  List<SimpleVkUser> loadUsers(List<Integer> userIds);
+  RelationList loadRelations(int userId);
+  List<RelationChange> buildChangesForNewUser(int userId);
 
-    SimpleVkUser loadUser(int userId);
-
-    List<SimpleVkUser> loadUsers(List<Integer> userIds);
-
-    List<RelationChange> buildChangesForNewUser(int usedId);
+  CompletableFuture<List<Integer>> loadFriendsAsync(int userId);
+  CompletableFuture<List<Integer>> loadFollowersAsync(int userId);
+  CompletableFuture<SimpleVkUser> loadUserAsync(int userId);
+  CompletableFuture<List<SimpleVkUser>> loadUsersAsync(List<Integer> userIds);
+  CompletableFuture<List<RelationChange>> buildChangesForNewUserAsync(int userId);
+  CompletableFuture<RelationList> loadRelationsAsync(int userId);
 }
--- src/main/java/musin/seeker/vkseeker/api/VkApiImpl.java
+++ src/main/java/musin/seeker/vkseeker/api/VkApiImpl.java
@@ -11,10 +11,13 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import static com.vk.api.sdk.client.Lang.EN;
-import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
+import static java.util.concurrent.CompletableFuture.allOf;
 import static java.util.stream.Collectors.toList;
 import static musin.seeker.vkseeker.db.model.RelationType.FOLLOWER;
 import static musin.seeker.vkseeker.db.model.RelationType.FRIEND;
@@ -22,87 +25,115 @@ import static musin.seeker.vkseeker.db.model.RelationType.FRIEND;
 @Service
 public class VkApiImpl implements VkApi {
 
-    private static final int LOAD_FOLLOWERS_TRIES = 3;
+  private final VkApiClient vkApi;
+  private final UserActor userActor;
+  private final Map<Integer, SimpleVkUser> usersCache = new HashMap<>();
+  private final Executor executor = Executors.newFixedThreadPool(10);
 
-    private final VkApiClient vkApi;
-    private final UserActor userActor;
-    private final Map<Integer, SimpleVkUser> usersCache = new HashMap<>();
+  public VkApiImpl(TransportClient transportClient) {
+    vkApi = new VkApiClient(transportClient);
+    userActor = new MusinUserActor();
+  }
 
-    public VkApiImpl(TransportClient transportClient) {
-        vkApi = new VkApiClient(transportClient);
-        userActor = new MusinUserActor();
-    }
+  @Override
+  @SneakyThrows
+  public List<Integer> loadFriends(int userId) {
+    return vkApi.friends()
+        .get(userActor)
+        .userId(userId)
+        .lang(EN)
+        .execute()
+        .getItems();
+  }
 
-    @Override
-    @SneakyThrows
-    public List<Integer> loadFriends(int userId) {
-        return vkApi.friends()
-                .get(userActor)
-                .userId(userId)
-                .lang(EN)
-                .execute()
-                .getItems();
-    }
+  @SneakyThrows
+  @Override
+  public List<Integer> loadFollowers(int userId) {
+    return vkApi.users()
+        .getFollowers(userActor)
+        .count(1000)
+        .userId(userId)
+        .lang(EN)
+        .execute()
+        .getItems();
+  }
 
-    @Override
-    public List<Integer> loadFollowers(int userId) {
-        for (int i = 0; i < LOAD_FOLLOWERS_TRIES; i++) {
-            List<Integer> followers = loadFollowers0(userId);
-            if (!followers.isEmpty()) return followers;
-        }
-        return emptyList();
-    }
+  @Override
+  public SimpleVkUser loadUser(int userId) {
+    return loadUsers(singletonList(userId)).get(0);
+  }
 
-    @SneakyThrows
-    private List<Integer> loadFollowers0(int userId) {
-        return vkApi.users()
-                .getFollowers(userActor)
-                .count(1000)
-                .userId(userId)
-                .lang(EN)
-                .execute()
-                .getItems();
-    }
+  @Override
+  public List<SimpleVkUser> loadUsers(List<Integer> userIds) {
+    loadUsersToCache(userIds);
+    return userIds.stream()
+        .map(usersCache::get)
+        .collect(toList());
+  }
 
-    @Override
-    public SimpleVkUser loadUser(int userId) {
-        return loadUsers(singletonList(userId)).get(0);
-    }
+  @SneakyThrows
+  private void loadUsersToCache(List<Integer> userIds) {
+    List<String> asString = userIds.stream()
+        .filter(id -> !usersCache.containsKey(id))
+        .distinct()
+        .map(Object::toString)
+        .collect(toList());
+    if (asString.isEmpty()) return;
+    vkApi.users()
+        .get(userActor)
+        .userIds(asString)
+        .lang(EN)
+        .execute()
+        .forEach(u -> usersCache.put(
+            u.getId(),
+            new SimpleVkUser(u.getId(), u.getFirstName(), u.getLastName()))
+        );
+  }
 
-    @Override
-    public List<SimpleVkUser> loadUsers(List<Integer> userIds) {
-        loadUsersToCache(userIds);
-        return userIds.stream()
-                .map(usersCache::get)
-                .collect(toList());
-    }
+  @Override
+  public RelationList loadRelations(int userId) {
+    return loadRelationsAsync(userId).join();
+  }
 
-    @SneakyThrows
-    private void loadUsersToCache(List<Integer> userIds) {
-        List<String> asString = userIds.stream()
-                .filter(id -> !usersCache.containsKey(id))
-                .distinct()
-                .map(Object::toString)
-                .collect(toList());
-        if (asString.isEmpty()) return;
-        vkApi.users()
-                .get(userActor)
-                .userIds(asString)
-                .lang(EN)
-                .execute()
-                .forEach(u -> usersCache.put(
-                        u.getId(),
-                        new SimpleVkUser(u.getId(), u.getFirstName(), u.getLastName()))
-                );
-    }
+  @Override
+  public List<RelationChange> buildChangesForNewUser(int userId) {
+    return loadRelations(userId).getActiveChanges();
+  }
 
-    @Override
-    public List<RelationChange> buildChangesForNewUser(int userId) {
-        final List<Integer> friends = loadFriends(userId);
-        final List<Integer> followers = loadFollowers(userId);
-        final RelationList list = new RelationList(userId);
-        friends.forEach(id -> list.applyChange(RelationChange.builder().curType(FRIEND).target(id).build()));
-        followers.forEach(id -> list.applyChange(RelationChange.builder().curType(FOLLOWER).target(id).build()));
-        return list.getActiveChanges();
-    }
+  @Override
+  public CompletableFuture<List<Integer>> loadFriendsAsync(int userId) {
+    return CompletableFuture.supplyAsync(() -> loadFriends(userId), executor);
+  }
+
+  @Override
+  public CompletableFuture<List<Integer>> loadFollowersAsync(int userId) {
+    return CompletableFuture.supplyAsync(() -> loadFollowers(userId), executor);
+  }
+
+  @Override
+  public CompletableFuture<SimpleVkUser> loadUserAsync(int userId) {
+    return CompletableFuture.supplyAsync(() -> loadUser(userId), executor);
+  }
+
+  @Override
+  public CompletableFuture<List<SimpleVkUser>> loadUsersAsync(List<Integer> userIds) {
+    return CompletableFuture.supplyAsync(() -> loadUsers(userIds), executor);
+  }
+
+  @Override
+  public CompletableFuture<List<RelationChange>> buildChangesForNewUserAsync(int userId) {
+    return CompletableFuture.supplyAsync(() -> buildChangesForNewUser(userId), executor);
+  }
+
+  @Override
+  public CompletableFuture<RelationList> loadRelationsAsync(int userId) {
+    CompletableFuture<List<Integer>> friends = loadFriendsAsync(userId);
+    CompletableFuture<List<Integer>> followers = loadFollowersAsync(userId);
+    return allOf(friends, followers).thenApply(v -> {
+      RelationList list = new RelationList(userId);
+      friends.join().forEach(id -> list.applyChange(RelationChange.builder().curType(FRIEND).target(id).build()));
+      followers.join().forEach(id -> list.applyChange(RelationChange.builder().curType(FOLLOWER).target(id).build()));
+      return list;
+    });
+  }
 }
