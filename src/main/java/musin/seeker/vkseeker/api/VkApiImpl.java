7a5293388f71f73275f4bd7b1bc45c3216fa83ab
Rustam Musin <PloadyFree@gmail.com>
2020-06-06 19:26:47 +0400

    Simplify loadRelationsAsync in VkApiImpl

--- src/main/java/musin/seeker/vkseeker/api/VkApiImpl.java
+++ src/main/java/musin/seeker/vkseeker/api/VkApiImpl.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import musin.seeker.vkseeker.RelationList;
 import musin.seeker.vkseeker.db.model.RelationChange;
+import musin.seeker.vkseeker.db.model.RelationType;
 import org.springframework.core.task.AsyncListenableTaskExecutor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 
 import static com.vk.api.sdk.client.Lang.EN;
 import static java.util.Collections.singletonList;
-import static java.util.concurrent.CompletableFuture.allOf;
 import static java.util.stream.Collectors.toList;
 import static musin.seeker.vkseeker.db.model.RelationType.FOLLOWER;
 import static musin.seeker.vkseeker.db.model.RelationType.FRIEND;
@@ -29,6 +30,18 @@ public class VkApiImpl implements VkApi {
   private final UserActor userActor;
   private final AsyncListenableTaskExecutor taskExecutor;
   private final Map<Integer, SimpleVkUser> usersCache = new ConcurrentHashMap<>();
+
+  private static RelationChange buildNewChange(int id, RelationType type) {
+    return RelationChange.builder().curType(type).target(id).build();
+  }
+
+  private static RelationChange buildNewFriend(int id) {
+    return buildNewChange(id, FRIEND);
+  }
+
+  private static RelationChange buildNewFollower(int id) {
+    return buildNewChange(id, FOLLOWER);
+  }
 
   @Override
   @SneakyThrows
@@ -112,13 +125,8 @@ public class VkApiImpl implements VkApi {
 
   @Override
   public CompletableFuture<RelationList> loadRelationsAsync(int userId) {
-    CompletableFuture<List<Integer>> friends = loadFriendsAsync(userId);
-    CompletableFuture<List<Integer>> followers = loadFollowersAsync(userId);
-    return allOf(friends, followers).thenApply(v -> {
-      RelationList list = new RelationList(userId);
-      friends.join().forEach(id -> list.applyChange(RelationChange.builder().curType(FRIEND).target(id).build()));
-      followers.join().forEach(id -> list.applyChange(RelationChange.builder().curType(FOLLOWER).target(id).build()));
-      return list;
-    });
+    var friends = loadFriendsAsync(userId).thenApply(s -> s.stream().map(VkApiImpl::buildNewFriend));
+    var followers = loadFollowersAsync(userId).thenApply(s -> s.stream().map(VkApiImpl::buildNewFollower));
+    return friends.thenCombine(followers, Stream::concat).thenApply(s -> new RelationList(userId, s::iterator));
   }
 }
