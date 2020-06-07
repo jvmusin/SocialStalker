7b06ad5257f5a240480adf2cff276e070c3ba23d
Rustam Musin <PloadyFree@gmail.com>
2020-06-07 19:34:13 +0400

    Make telegram meetings truly async by using TaskExecutor in sendMessage

--- src/main/java/musin/seeker/vkseeker/notifiers/TelegramChangesNotifier.java
+++ src/main/java/musin/seeker/vkseeker/notifiers/TelegramChangesNotifier.java
@@ -3,7 +3,7 @@ package musin.seeker.vkseeker.notifiers;
 import musin.seeker.vkseeker.api.VkApi;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -20,19 +20,22 @@ public class TelegramChangesNotifier extends ChangesNotifierBase {
   private static final String BOT_TOKEN = "809103789:AAFReBbzwDxrpVCFLJ2JZv2EKcaaAJuqP6o";
 
   private final RestTemplate restTemplate;
+  private final TaskExecutor taskExecutor;
 
-  public TelegramChangesNotifier(VkApi vkApi, @Qualifier("tgRestTemplate") RestTemplate restTemplate) {
+  public TelegramChangesNotifier(VkApi vkApi, @Qualifier("tgRestTemplate") RestTemplate restTemplate, TaskExecutor taskExecutor) {
     super(vkApi);
     this.restTemplate = restTemplate;
+    this.taskExecutor = taskExecutor;
   }
 
   @Override
-  @Async
   public void sendMessage(String message) {
-    SendMessage m = new SendMessage(MUSIN_UID, message);
-    m.setParseMode("Markdown");
-    String url = String.format("https://api.telegram.org/bot%s/sendMessage", BOT_TOKEN);
-    restTemplate.postForEntity(url, m, Message.class);
+    taskExecutor.execute(() -> {
+      SendMessage m = new SendMessage(MUSIN_UID, message);
+      m.setParseMode("Markdown");
+      String url = String.format("https://api.telegram.org/bot%s/sendMessage", BOT_TOKEN);
+      restTemplate.postForEntity(url, m, Message.class);
+    });
   }
 
   @PostConstruct
