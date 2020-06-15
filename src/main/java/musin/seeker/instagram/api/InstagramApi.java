b3c11a56283d328359467efc8a3112e8fc241d14
Rustam Musin <PloadyFree@gmail.com>
2020-06-15 23:06:23 +0400

    Drop some unused interfaces

--- src/main/java/musin/seeker/instagram/api/InstagramApi.java
+++ src/main/java/musin/seeker/instagram/api/InstagramApi.java
@@ -1,12 +1,76 @@
 package musin.seeker.instagram.api;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.brunocvcunha.instagram4j.Instagram4j;
+import org.brunocvcunha.instagram4j.requests.InstagramGetUserFollowersRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramGetUserFollowingRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramGetUserInfoRequest;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramGetUserFollowersResult;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramSearchUsernameResult;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramUser;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramUserSummary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
-public interface InstagramApi {
-  InstagramApiUser loadUser(long userId);
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.stream.Collectors.toList;
 
-  CompletableFuture<List<Long>> loadFollowers(long userId);
+@Component
+@Profile("instagram")
+@RequiredArgsConstructor
+//todo add restriction to 200 requests per hour
+public class InstagramApi {
 
-  CompletableFuture<List<Long>> loadFollowing(long userId);
+  private final Instagram4j instagram;
+  private final Map<Long, InstagramApiUser> users = new ConcurrentHashMap<>();
+
+  private InstagramApiUser mapUser(InstagramUserSummary user) {
+    return new InstagramApiUser(user.pk, user.username, user.full_name);
+  }
+
+  private InstagramApiUser mapUser(InstagramUser user) {
+    return new InstagramApiUser(user.pk, user.username, user.full_name);
+  }
+
+  private void saveUser(long pk, InstagramApiUser user) {
+    users.put(pk, user);
+  }
+
+  public InstagramApiUser loadUser(long userId) {
+    //noinspection Convert2Lambda
+    return users.computeIfAbsent(userId, new Function<>() {
+      @Override
+      @SneakyThrows
+      public InstagramApiUser apply(Long id) {
+        InstagramSearchUsernameResult res = instagram.sendRequest(new InstagramGetUserInfoRequest(id));
+        return mapUser(res.getUser());
+      }
+    });
+  }
+
+  private List<Long> followersToIds(List<InstagramUserSummary> followers) {
+    return followers.stream()
+        .peek(u -> saveUser(u.pk, mapUser(u)))
+        .map(u -> u.pk)
+        .collect(toList());
+  }
+
+  @SneakyThrows
+  public CompletableFuture<List<Long>> loadFollowers(long userId) {
+    InstagramGetUserFollowersResult followers = instagram.sendRequest(new InstagramGetUserFollowersRequest(userId));
+    return completedFuture(followersToIds(followers.getUsers()));
+  }
+
+  @SneakyThrows
+  public CompletableFuture<List<Long>> loadFollowing(long userId) {
+    InstagramGetUserFollowersResult following = instagram.sendRequest(new InstagramGetUserFollowingRequest(userId));
+    return completedFuture(followersToIds(following.getUsers()));
+  }
 }
--- src/main/java/musin/seeker/vk/api/VkApi.java
+++ src/main/java/musin/seeker/vk/api/VkApi.java
@@ -1,12 +1,81 @@
 package musin.seeker.vk.api;
 
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
-public interface VkApi {
-  SimpleVkUser loadUser(int userId);
+import static com.vk.api.sdk.client.Lang.EN;
+import static java.util.Collections.singleton;
+import static java.util.stream.Collectors.toList;
 
-  CompletableFuture<List<Integer>> loadFriendsAsync(int userId);
+@Service
+@RequiredArgsConstructor
+public class VkApi {
 
-  CompletableFuture<List<Integer>> loadFollowersAsync(int userId);
+  private final VkApiClient vkApiClient;
+  private final UserActor userActor;
+  private final AsyncListenableTaskExecutor taskExecutor;
+  private final Map<Integer, SimpleVkUser> usersCache = new ConcurrentHashMap<>();
+
+  public SimpleVkUser loadUser(int userId) {
+    return loadUsers(singleton(userId)).get(0);
+  }
+
+  private List<SimpleVkUser> loadUsers(Set<Integer> userIds) {
+    loadUsersToCache(userIds);
+    return userIds.stream()
+        .map(usersCache::get)
+        .collect(toList());
+  }
+
+  @SneakyThrows
+  private void loadUsersToCache(Set<Integer> userIds) {
+    List<String> asString = userIds.stream()
+        .filter(id -> !usersCache.containsKey(id))
+        .distinct()
+        .map(Object::toString)
+        .collect(toList());
+    if (asString.isEmpty()) return;
+    vkApiClient.users()
+        .get(userActor)
+        .userIds(asString)
+        .lang(EN)
+        .execute()
+        .forEach(u -> usersCache.put(
+            u.getId(),
+            new SimpleVkUser(u.getId(), u.getFirstName(), u.getLastName()))
+        );
+  }
+
+  public CompletableFuture<List<Integer>> loadFriendsAsync(int userId) {
+    return taskExecutor.submitListenable(() -> vkApiClient
+        .friends()
+        .get(userActor)
+        .userId(userId)
+        .lang(EN)
+        .execute()
+        .getItems()
+    ).completable();
+  }
+
+  public CompletableFuture<List<Integer>> loadFollowersAsync(int userId) {
+    return taskExecutor.submitListenable(() -> vkApiClient
+        .users()
+        .getFollowers(userActor)
+        .count(1000)
+        .userId(userId)
+        .lang(EN)
+        .execute()
+        .getItems()
+    ).completable();
+  }
 }
